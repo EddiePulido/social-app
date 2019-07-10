24690493ecbfaadc39102c98d959a724a949b8e7
EddiePulido <47019731+EddiePulido@users.noreply.github.com>
2019-07-10 12:56:06 -0700

    Delete config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyDWmn-OsBZoI5THSSRsFZrZJ6da3W88lng",
-    authDomain: "social-4a2a0.firebaseapp.com",
-    databaseURL: "https://social-4a2a0.firebaseio.com",
-    projectId: "social-4a2a0",
-    storageBucket: "social-4a2a0.appspot.com",
-    messagingSenderId: "816648710597",
-    appId: "1:816648710597:web:cfdae25ad11c055c"
-  };
